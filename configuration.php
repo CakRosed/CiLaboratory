7e15b7baaff74dbfb517edc5d417cf20613ef55e
rosid <aunur.rosidin@gmail.com>
2019-02-04 04:35:51 -0800

    pembuatan base library

--- configuration.php
+++ configuration.php
@@ -1,7 +1,17 @@
 <?php 
 
     class SConfig{
-
+        var $_site_url          = "http://localhost/cilaboratory/";
+        var $_document_root     = "C:\xampp\htdocs\CiLaboratory";
+        var $_host_name         = "localhost";
+        var $_database_name     = "cicms";
+        var $_site_name         = "lehodev.com";
+        var $_database_user     = "root";
+        var $_database_password = "";
+        var $_table_prefix      = "tbl_";
+        var $_backend_template  = "default";
+        var $_backend_perpage   = 10;
+        var $_frontend_perpage  = 5;
     }
 
-?>
+?> 
--- dev/application/controllers/Welcome.php
+++ dev/application/controllers/Welcome.php
@@ -19,17 +19,7 @@ class Welcome extends Frontend_Controller {
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index(){
-		$data = array(
-			'username' 	=> 'rosidin',
-			'password' 	=> 'anonymouse135',
-			'group' 	=> 'admin',
-			'email' 	=> 'aunur.rosidin@gmail.com'
-		);
-
-		$delete = $this->User_model->delete(5);
-		if ($delete) {
-			echo "sukses";
-		}
+		echo base_url();
 		$this->load->view('welcome_message');
 	}
 }
